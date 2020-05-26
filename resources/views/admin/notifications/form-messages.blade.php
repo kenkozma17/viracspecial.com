89784420cf82ff59c970a3f1b155104022ce1a94
Ken Kozma <kkozma@liquidfish.com>
2020-05-25 19:12:43 -0500

    Added it again

--- resources/views/admin/notifications/form-messages.blade.php
+++ resources/views/admin/notifications/form-messages.blade.php
@@ -14,7 +14,7 @@
 @endif
 
 @if (Session::get('successMessage'))
-    <div class="container mt-lg-5">
+    <div class="container content-mt">
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             <p>{{ Session::get('successMessage') }}</p>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
