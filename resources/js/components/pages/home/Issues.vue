7de2947569e5661ccda15fb043df4dfa1399a3a9
Ken Kozma <kenkozma17@gmail.com>
2021-07-17 10:06:59 -0500

    Compiling assets

--- resources/js/components/pages/home/Issues.vue
+++ resources/js/components/pages/home/Issues.vue
@@ -4,27 +4,9 @@
             <div class="title">Newsletter Issues</div>
             <div class="swiper-container swiper-multi">
                 <div class="swiper-wrapper">
-                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1sBUkFeD5EaqRCJtxFJoZkVjs4D3DOsvH/view?usp=sharing">
-                        <div class="image" style="background-image: url('/images/Issues/Issue1.jpg')">
-                            <span class="issue-date">July - Sept 2018</span>
-                            <img class="issue-logo" src="/images/Logo.png">
-                        </div>
-                    </a>
-                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1ZoLKD4TM652BBQHR6uG3tCyIlEEqKiy5/view?usp=sharing">
-                        <div class="image" style="background-image: url('/images/Issues/Issue2.jpg')">
-                            <span class="issue-date">Oct - Dec 2018</span>
-                            <img class="issue-logo" src="/images/Logo.png">
-                        </div>
-                    </a>
-                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1sn1NyEzU3QGtiS0KIm8EZgr_AvkYeW23/view?usp=sharing">
-                        <div  class="image" style="background-image: url('/images/Issues/Issue3.jpg')">
-                            <span class="issue-date">Jan - Mar 2019</span>
-                            <img class="issue-logo" src="/images/Logo.png">
-                        </div>
-                    </a>
-                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1vfq5pztubPte5PXFnxXRuN7bGN-rxGtM/view?usp=sharing">
-                        <div  class="image" style="background-image: url('/images/Issues/Issue4.jpg')">
-                            <span class="issue-date">Apr - Sept 2019</span>
+                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/14_G2lYo5ShmQemyAMs31pn9gU-h_x8jA/view">
+                        <div  class="image" style="background-image: url('/images/Issues/Issue5.jpg')">
+                            <span class="issue-date">January - June 2021</span>
                             <img class="issue-logo" src="/images/Logo.png">
                         </div>
                     </a>
@@ -34,9 +16,27 @@
                             <img class="issue-logo" src="/images/Logo.png">
                         </div>
                     </a>
-                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/14_G2lYo5ShmQemyAMs31pn9gU-h_x8jA/view">
-                        <div  class="image" style="background-image: url('/images/Issues/Issue5.jpg')">
-                            <span class="issue-date">January - June 2021</span>
+                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1vfq5pztubPte5PXFnxXRuN7bGN-rxGtM/view?usp=sharing">
+                        <div  class="image" style="background-image: url('/images/Issues/Issue4.jpg')">
+                            <span class="issue-date">Apr - Sept 2019</span>
+                            <img class="issue-logo" src="/images/Logo.png">
+                        </div>
+                    </a>
+                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1sn1NyEzU3QGtiS0KIm8EZgr_AvkYeW23/view?usp=sharing">
+                        <div  class="image" style="background-image: url('/images/Issues/Issue3.jpg')">
+                            <span class="issue-date">Jan - Mar 2019</span>
+                            <img class="issue-logo" src="/images/Logo.png">
+                        </div>
+                    </a>
+                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1ZoLKD4TM652BBQHR6uG3tCyIlEEqKiy5/view?usp=sharing">
+                        <div class="image" style="background-image: url('/images/Issues/Issue2.jpg')">
+                            <span class="issue-date">Oct - Dec 2018</span>
+                            <img class="issue-logo" src="/images/Logo.png">
+                        </div>
+                    </a>
+                    <a class="swiper-slide" target="_blank" href="https://drive.google.com/file/d/1sBUkFeD5EaqRCJtxFJoZkVjs4D3DOsvH/view?usp=sharing">
+                        <div class="image" style="background-image: url('/images/Issues/Issue1.jpg')">
+                            <span class="issue-date">July - Sept 2018</span>
                             <img class="issue-logo" src="/images/Logo.png">
                         </div>
                     </a>
